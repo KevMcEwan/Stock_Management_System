cc0899baffdbaed8b452482b10805c55c9493428
KevMcEwan <kev_mcewan@hotmail.com>
2018-11-12 15:08:51 +0000

    rearranged folder structure for ease of use

--- Shop Inventory/controller.rb
+++ Shop Inventory/controller.rb
@@ -12,21 +12,21 @@ end
 
 get '/products' do
   @products = Product.all
-  erb( :products)
+  erb( :"products/products")
 end
 
-get '/products/stock_order' do
+get '/products/re-stock_required' do
   @products = Product.stock_order
-  erb( :stock_order)
+  erb( :"products/re-stock_required")
 end
 
 get '/products/profit_margin' do
   @products = Product.all
-  erb( :profit_margin)
+  erb( :"products/profit_margin")
 end
 
 get '/products/add_new_product' do
-  erb( :"add_new_product" )
+  erb( :"products/add_new_product" )
 end
 
 post '/products' do
@@ -37,59 +37,66 @@ end
 
 get '/products/:id' do
   @product = Product.find( params[:id] )
-  erb( :"/productinfo" )
+  erb( :"/products/productinfo" )
 end
+
+
+post '/products/:id' do
+  Product.new(params).update
+  redirect to '/products/profit_margin'
+end
+
 
 get '/products/:id/editproduct' do
   @product = Product.find( params[:id] )
-  erb( :editproduct )
+  erb( :"products/editproduct" )
 end
 
 post '/products/:id' do
   Product.new(params).update
-  redirect to '/products'
+  redirect to '/products/products'
 end
 
 post '/products/:id/delete' do
   product = Product.find( params[:id] )
   product.delete()
-  redirect to '/products'
+  redirect to '/products/products'
 end
 
 
 
 get '/suppliers' do
   @suppliers = Supplier.all
-  erb (:suppliers)
+  erb (:"suppliers/suppliers")
 end
 
 get '/suppliers/add_new_supplier' do
-  erb( :"add_new_supplier" )
+  erb( :"suppliers/add_new_supplier" )
 end
 
 post '/suppliers' do
   @supplier = Supplier.new( params )
   @supplier.save
-  redirect to '/suppliers'
+  redirect to '/suppliers/suppliers'
 end
 
 get '/suppliers/:id' do
   @supplier = Supplier.find( params[:id] )
-  erb( :"/supplier_info" )
+  erb( :"/suppliers/supplier_info" )
 end
 
 get '/suppliers/:id/editsupplier' do
   @supplier = Supplier.find( params[:id] )
-  erb( :editsupplier )
+  erb( :"suppliers/editsupplier" )
 end
 
 post '/suppliers/:id' do
   Supplier.new(params).update
-  redirect to '/suppliers'
+  redirect to '/suppliers/suppliers'
 end
 
 post '/suppliers/:id/delete' do
   supplier = Supplier.find( params[:id] )
   supplier.delete()
-  redirect to '/suppliers'
+  redirect to '/suppliers/suppliers'
 end
